f5eb6bc802ba3f3489a9ed26922e0090dd564ecb
Amrutha <amruthaa.v21@gmail.com>
2018-12-19 14:49:27 +0530

    Update view

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "sample_app_#{Rails.env}"
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'fathomless-reef-52698.herokuapp.com'
+  host = 'https://fathomless-reef-52698.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
