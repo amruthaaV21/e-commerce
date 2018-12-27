67f8479f2c3b7691d13643ddfcf748a2951c91a6
Amrutha <amruthaa.v21@gmail.com>
2018-12-27 22:23:44 +0530

    modify destroy products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
 
   before_action :logged_in_user, only: [:create, :destroy]
-  before_action :admin_user,   only: :destroy
+  before_action :admin_or_correct_user,   only: :destroy
 
  def new
    # @user = User.find(params[:user])
@@ -35,9 +35,11 @@ class ProductsController < ApplicationController
   end
 
   def destroy
+    
     Product.find(params[:id]).destroy
     flash[:success] = "Product deleted"
     redirect_to request.referrer || root_url
+    
   end
 
   private
@@ -46,9 +48,11 @@ class ProductsController < ApplicationController
       params.require(:product).permit(:name, :category, :price, :quantity, :discount, :picture)
     end
 
-   def correct_user
+   def admin_or_correct_user
+      
+      if !current_user.try(:admin?)
       @product = current_user.products.find_by(id: params[:id])
       redirect_to root_url if @product.nil?
-    end
-
+      end
+  end
 end
